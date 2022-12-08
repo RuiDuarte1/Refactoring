951e0d651be8f6cb10348dc87b705324ea7a95ea
Rui Duarte <ruycruzeiro9@gmail.com>
2022-12-08 13:40:29 -0300

    Commit 18

--- src/refactoring/Statement.java
+++ src/refactoring/Statement.java
@@ -1,53 +1,10 @@
 package refactoring;
-
-import java.util.Dictionary;
 import java.util.Enumeration;
 
+public abstract class Statement {
+    public abstract String statement(Enumeration<Rental> rentals, Customer customer);
 
-
-public class Statement {
-    public String statement() {
-        Dictionary<Object, Object> _rentals = null;
-        Enumeration rentals = _rentals.elements();
-        String result = "Rental Record for " + getName() + "\n";
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-
-            // show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" +
-                    String.valueOf(each.getCharge()) + "\n";
-        }
-
-        // add footer lines
-        result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
-                " frequent renter points";
-        return result;
-    }
-
-    public String getName() {
-        return null;
-    }
-
-    public double getTotalCharge() {
-        double result = 0;
-        Dictionary<Object, Object> _rentals = null;
-        Enumeration rentals = _rentals.elements();
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            result += each.getCharge();
-        }
-        return result;
-    }
-
-    public int getTotalFrequentRenterPoints(){
-        int result = 0;
-        Dictionary<Object, Object> _rentals = null;
-        Enumeration rentals = _rentals.elements();
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-            result += each.getFrequentRenterPoints();
-        }
-        return result;
+    public String value(Customer aCustomer) {
+        return this.statement(aCustomer.getRentals(), aCustomer);
     }
 }
