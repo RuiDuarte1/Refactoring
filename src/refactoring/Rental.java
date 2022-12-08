bf0ebd516b5b4c8d2df04339df21e87aca0326f3
Rui Duarte <ruycruzeiro9@gmail.com>
2022-12-08 10:11:14 -0300

    Commit 4

--- src/refactoring/Rental.java
+++ src/refactoring/Rental.java
@@ -3,6 +3,7 @@ package refactoring;
 public class Rental {
     private Movie _movie;
     private int _daysRented;
+    private Rental each;
 
     public Rental(Movie movie, int daysRented) {
         _movie = movie;
@@ -15,5 +16,26 @@ public class Rental {
 
     public Movie getMovie() {
         return _movie;
+    }
+
+    public double getCharge() {
+        double thisAmount = 0;
+        switch (each.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (each.getDaysRented() > 2)
+                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (each.getDaysRented() > 3)
+                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                break;
+        }
+
+        return thisAmount;
     }
 }
